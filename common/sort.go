582cde4fe3107bfed3e31b8960ae080bf2f54fc9
Louch2010 <1175889450@qq.com>
2016-10-12 15:19:27 +0800

    sort set 调试

--- common/sort.go
+++ common/sort.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"fmt"
+	"strconv"
 )
 
 //有序项
@@ -16,16 +17,20 @@ type SortSet struct {
 	items  []*SortItem
 }
 
+func NewSortSet() SortSet {
+	return SortSet{
+		length: 0,
+		items:  make([]*SortItem, 10),
+	}
+}
+
 //长度
-func (l SortSet) Len() int {
+func (l *SortSet) Len() int {
 	return l.length
 }
 
 //增加
-func (l SortSet) Add(score int, data interface{}) {
-	fmt.Println("-------------")
-	fmt.Println("长度为：", l.Len())
-	fmt.Println("容量为：", len(l.items))
+func (l *SortSet) Add(score int, data interface{}) {
 	tmp := &SortItem{
 		Score: score,
 		Data:  data,
@@ -34,56 +39,59 @@ func (l SortSet) Add(score int, data interface{}) {
 }
 
 //增加元素
-func (l SortSet) AddItem(item *SortItem) {
-	if l.Len() == 0 {
-		fmt.Println("0000000")
+func (l *SortSet) AddItem(item *SortItem) {
+	if l.length == 0 {
 		l.length++
-		l.items = make([]*SortItem, 10)
 		l.items[0] = item
-		fmt.Println("长度为：", l.Len())
-		fmt.Println("容量为：", len(l.items))
 		return
 	}
 	//扩容
-	if l.length == l.Len() {
+	if len(l.items)-1 <= l.Len() {
 		tmp := l.items
 		//扩为2倍
-		//l.items = [len(tmp) * 2]*SortItem{}
 		l.items = make([]*SortItem, len(tmp)*2)
 		//复制
 		for i := 0; i < len(tmp); i++ {
 			l.items[i] = tmp[i]
 		}
+		fmt.Println("扩容后容量：", len(l.items))
 	}
 	//排序
 	for i := 0; i < l.length; i++ {
-		if l.items[i].Score > item.Score {
+		//如果原数组最后一个值比要插入的值小，则要插入的值放在最后即可，且跳出循环
+		if i == l.length-1 && l.items[i].Score < item.Score {
+			l.items[i+1] = item
+			break
+		}
+		//如果当前值比要插入的值大，则从当前值开始，将数组剩余的部分全部后移一位，且将要插入的值放在当前位置
+		if l.items[i].Score >= item.Score {
 			//复制
 			for j := l.length; j > i; j-- {
 				l.items[j] = l.items[j-1]
 			}
 			l.items[i] = item
+			break
 		}
 	}
 	l.length++
 }
 
 //获取元素
-func (l SortSet) GetItem(index int) *SortItem {
-	if index >= l.Len() {
+func (l *SortSet) GetItem(index int) *SortItem {
+	if index >= l.length {
 		return nil
 	}
 	return l.items[index]
 }
 
 //获取内容
-func (l SortSet) GetItems() []*SortItem {
+func (l *SortSet) GetItems() []*SortItem {
 	return l.items[0:l.length]
 }
 
 //根据index删除
-func (l SortSet) RemoveItem(index int) *SortItem {
-	if index >= l.Len() {
+func (l *SortSet) RemoveItem(index int) *SortItem {
+	if index >= l.length {
 		return nil
 	}
 	item := l.items[index]
@@ -94,10 +102,10 @@ func (l SortSet) RemoveItem(index int) *SortItem {
 }
 
 //根据内容删除
-func (l SortSet) RemoveData(data interface{}) *SortItem {
+func (l *SortSet) RemoveData(data interface{}) *SortItem {
 	index := 0
 	var item *SortItem
-	for i := 0; i < l.Len(); i++ {
+	for i := 0; i < l.length; i++ {
 		if l.items[i].Data == data {
 			index = i
 			item = l.items[i]
@@ -105,8 +113,17 @@ func (l SortSet) RemoveData(data interface{}) *SortItem {
 		}
 	}
 	//将index后的数据进行复制
-	for i := index; i < l.Len(); i++ {
+	for i := index; i < l.length; i++ {
 		l.items[i] = l.items[i+1]
 	}
 	return item
+}
+
+func (l *SortSet) Test() {
+	//fmt.Println("容量为：", len(l.items), "，长度为：", l.length)
+	sb := ""
+	for i := 0; i < l.Len(); i++ {
+		sb = sb + strconv.Itoa(l.items[i].Score) + ","
+	}
+	fmt.Println("items:", sb)
 }
--- common/sort_test.go
+++ common/sort_test.go
@@ -2,16 +2,23 @@ package common
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
 	"testing"
 )
 
 func TestSort(t *testing.T) {
-	list := SortSet{}
-	list.Add(3, "cccc")
-	list.Add(4, "dddd")
-	list.Add(2, "bbbb")
-	list.Add(1, "aaaa")
-	list.Add(5, "eeee")
+	list := NewSortSet()
+	//	list.Add(3, "cccc")
+	//	list.Add(2, "bbbb2")
+	//	list.Add(4, "dddd")
+	//	list.Add(4, "dddd")
+	//	list.Add(2, "bbbb")
+	//	list.Add(1, "aaaa")
+	//	list.Add(5, "eeee")
+	for i := 0; i < 40; i++ {
+		list.Add(rand.Intn(20), "v_"+strconv.Itoa(i))
+	}
 	for index, item := range list.GetItems() {
 		fmt.Println(index, " = ", item)
 	}
